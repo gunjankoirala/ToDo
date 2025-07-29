6423a20d28da33cefd1e74379adb302fee0b89b5
Gunjan koirala <gunjankoirala60@gmail.com>
2025-07-29 16:38:18 +0545

    chore(config): add drizzle.config.ts for DB migration setup and address feedback

--- src/utils/case.ts
+++ src/utils/case.ts
@@ -1,3 +1,4 @@
+import { mysqlTableCreator, varchar as _varchar, int as _int, boolean as _boolean } from 'drizzle-orm/mysql-core';
 export function toSnakeCase(str: string): string {
   return str.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`);
 }
